57cb0ae4cf62b3e1499bc0c90fbffeeb81d6d492
AnirudhSreeram <anirudhsreeram@gmail.com>
2020-07-05 00:15:57 +0530

    mdel saving

--- keras_intro.py
+++ keras_intro.py
@@ -9,6 +9,35 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+import os.path
+from tensorflow.keras.models import load_model
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j], horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 # Generate the data
 train_labels = []
@@ -96,3 +125,15 @@ for i in predictions:
 rounded_predictions = np.argmax(predictions, axis=-1)
 for i in rounded_predictions:
     print(i)
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+cm_plot_labels = ['no_side_efffects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+
+if os.path.isfile('/home/anirudh/Documents/Keras_tutorials/med_trial_model.h5') is False:
+    model.save('/home/anirudh/Documents/Keras_tutorials/med_trial_model.h5')
+
+new_model = load_model('/home/anirudh/Documents/Keras_tutorials/med_trial_model.h5')
+print(new_model.summary())
+print(new_model.get_weights())
+print(new_model.optimizer)
